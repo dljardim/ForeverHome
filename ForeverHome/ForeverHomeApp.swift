c55feb8c326eae842a129374360d80472a362618
dljardim <dljardim@outlook.com>
2025-04-04 10:55:15 -0500

    Initial Commit

---
+++ ForeverHome/ForeverHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeverHomeApp.swift
+//  ForeverHome
+//
+//  Created by Damian Jardim on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ForeverHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
